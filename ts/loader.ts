3eb96b1c41f0e173c7f9687638d16c04a1bb4630
谢英杰(YingjieXie)-顺丰科技技术集团 <xieyingjie@sf-express.com>
2023-12-12 16:13:32 +0800

    chore: 优化loader.ts

--- ts/loader.ts
+++ ts/loader.ts
@@ -1,10 +1,17 @@
 import fs from 'fs'
 import path from 'path'
-import {app} from '@electron/remote'
+import { app } from '@electron/remote'
+
+console.error(__dirname);
 
 let baseDir = app.getPath('exe')
-if(process.env.NODE_ENV === 'development') {
-    baseDir = path.join(__dirname, '../../../../../../../../node_modules/@fork-anyone/node-nim/sdk/lib')
+if (process.env.NODE_ENV === 'development') {
+    const index = __dirname.lastIndexOf('node_modules');
+    if (process.platform === 'darwin') {
+        baseDir = path.join(__dirname.slice(0, index) ,'node_modules/@fork-anyone/node-nim/sdk/lib')
+    } else {
+        baseDir = path.join(__dirname.slice(0, index),  'node_modules/@fork-anyone/node-nim/sdk/lib')
+    }
 }
 
 
