47ebd79f2715cf1bb9188823b9ed975e5e4554c8
谢英杰(YingjieXie)-顺丰科技技术集团 <xieyingjie@sf-express.com>
2023-12-18 11:49:18 +0800

    fix: node-nim node path error

--- ts/loader.ts
+++ ts/loader.ts
@@ -2,6 +2,5 @@ import fs from 'fs'
 import path from 'path'
 import { app } from '@electron/remote'
 
-
 // @ts-ignore
-export default __non_webpack_require__(path.join(app.getPath('exe'), 'node-nim.node'))
+export default __non_webpack_require__(path.join(path.dirname(app.getPath('exe')), 'node-nim.node'))
