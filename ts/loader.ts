6c76fb6112c6f359f603c2f1a9e4b796eaf8176d
谢英杰(YingjieXie)-顺丰科技技术集团 <xieyingjie@sf-express.com>
2024-01-03 10:11:32 +0800

    upgrade: 兼容webpack以及node引入

--- ts/loader.ts
+++ ts/loader.ts
@@ -1,6 +1,29 @@
-import fs from 'fs'
-import path from 'path'
-import { app } from '@electron/remote'
+import { app } from '@electron/remote';
+import path from 'path';
 
-// @ts-ignore
-export default __non_webpack_require__(path.join(path.dirname(app.getPath('exe')), 'node-nim.node'))
+let moduleRequire = null;
+if(typeof require !== 'undefined') {
+    moduleRequire = require;
+    
+}  // @ts-ignore 兼容webpack打包
+else if(typeof __non_webpack_require__ !== 'undefined' ) {
+     // @ts-ignore 兼容webpack打包
+    moduleRequire = __non_webpack_require__;
+}
+if(!moduleRequire) {
+    throw new Error('can not find require module.');
+}
+
+
+let nodePath = process.env.NODE_NIM_PATH;
+
+if(!nodePath) {
+    nodePath =  path.join(path.dirname(moduleRequire('@electron/remote').app.getPath('exe')), 'node-nim.node')
+}
+
+const mod = moduleRequire(nodePath);
+
+
+if(!mod) throw new Error(`can not find node-nim.node in ${nodePath}`);
+
+export default mod;
