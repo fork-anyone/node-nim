080e7fcb252c074ca1b3f30dad467337306b4c0c
谢英杰(YingjieXie)-顺丰科技技术集团 <xieyingjie@sf-express.com>
2024-01-23 11:07:08 +0800

    chore: 调整loader 适应丰声打包环境

--- ts/loader.ts
+++ ts/loader.ts
@@ -1,4 +1,4 @@
-import { app } from '@electron/remote';
+
 import path from 'path';
 
 let moduleRequire = null;
@@ -7,8 +7,7 @@ if(typeof __non_webpack_require__ !== 'undefined' ) {
     // @ts-ignore 兼容webpack打包
    moduleRequire = __non_webpack_require__;
 } else if(typeof require !== 'undefined') {
-    moduleRequire = require;
-    
+   moduleRequire = require;
 }  
 if(!moduleRequire) {
     throw new Error('can not find require module.');
@@ -18,7 +17,11 @@ if(!moduleRequire) {
 let nodePath = process.env.NODE_NIM_PATH;
 
 if(!nodePath) {
-    nodePath =  path.join(path.dirname(moduleRequire('@electron/remote').app.getPath('exe')), 'node-nim.node')
+    if(process.platform === 'darwin') {
+        nodePath =  path.join(path.join(process.execPath, '../../../../../'), 'MacOS/node-nim.node')
+    } else {
+        nodePath =  path.join(path.join(process.execPath, '../'), 'node-nim.node')
+    }
 }
 
 const mod = moduleRequire(nodePath);
